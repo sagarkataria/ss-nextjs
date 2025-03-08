25e61f821a954e102d73699f5a51a730ae625ad6
sagarkataria <sagarsingh0110@gmail.com>
2025-03-09 00:14:13 +0530

    Enhance sign-up functionality with username and email validation, and add verification email sending

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -7,7 +7,87 @@ export async function POST(request: Request) {
   await dbConnect();
   try {
     const { email, username, password } = await request.json();
-    
+
+    const existingUserVerifyByUsername = await UserModel.findOne({
+      username,
+      isVarified: true,
+    });
+
+    if (existingUserVerifyByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: 'Username already exists',
+        },
+        {
+          status: 400,
+        },
+      );
+    }
+    const existingUserByEmail = await UserModel.findOne({ email });
+    const verifyCode = Math.random().toString(36).substring(7);
+    if (existingUserByEmail) {
+      if (existingUserByEmail.isVarified) {
+        return Response.json(
+          {
+            success: false,
+            message: 'Email already exists',
+          },
+          {
+            status: 400,
+          },
+        );
+      }else{
+        const hashedPassword = await bcrypt.hash(password, 10);
+        existingUserByEmail.password = hashedPassword;
+        existingUserByEmail.varifyCode = verifyCode;
+        existingUserByEmail.varifyCodeExpire = new Date();
+        existingUserByEmail.varifyCodeExpire.setHours(existingUserByEmail.varifyCodeExpire.getHours() + 1);
+        await existingUserByEmail.save();
+      }
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+        varifyCode: verifyCode,
+        varifyCodeExpire: expiryDate,
+        isVarified: false,
+        isAcceptingMessages: true,
+        messages: [],
+      });
+      await newUser.save();
+    }
+    // send verification email
+    const emailResponse = await sendVerificationEmail(
+      email,
+      username,
+      verifyCode,
+    );
+    if (!emailResponse.success) {
+      return Response.json(
+        {
+          success: false,
+          message: emailResponse.message,
+        },
+        {
+          status: 500,
+        },
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: 'User created successfully',
+      },
+      {
+        status: 200,
+      },
+    );
   } catch (error) {
     console.log('Error signing up', error);
     return Response.json(
