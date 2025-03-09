c77085d78a1e2d854ac316c856809accca13593c
sagarkataria <sagarsingh0110@gmail.com>
2025-03-09 17:39:13 +0530

    Implement authentication flow with NextAuth, including session management and middleware for protected routes

---
+++ src/model/middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+export { default } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+
+  const url = request.nextUrl;
+  if (
+    token &&
+    (
+      url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify')) ||
+      url.pathname.startsWith('/'))
+
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith('/about')) {
+    return NextResponse.rewrite(new URL('/about-2', request.url));
+  }
+
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.rewrite(new URL('/dashboard/user', request.url));
+  }
+}
+
+export const config = {
+  matcher: ['/sign-in', '/sign-up', '/dashboard/:path*', '/', '/verify/:path*'],
+};
